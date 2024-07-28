82af78e2e73fd3c042c1be4352f8868aff64a700
connor-72 <connorjovanovic@gmail.com>
2024-07-27 20:05:52 -0400

    create main function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+function main () {
+    if (process.argv.length < 3) {
+        console.log('no website provided')
+        return
+    }
+    if (process.argv.length > 3) {
+        console.log('too many arguments provided')
+        return
+    }
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl of: ${baseURL}...`)
+}
+
+main()
